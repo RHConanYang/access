ae54985c3c6018071abfed5cb4bf69efe44dca98
Robert <millduos@gmail.com>
2020-02-24 11:16:56 +0800

    Initial Commit

---
+++ access/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  access
+//
+//  Created by 楊仁翰 on 2020/2/24.
+//  Copyright © 2020 Renhen Yang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
