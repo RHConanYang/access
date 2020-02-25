d33ac0dd106d733436686305656b08d79c6c05f3
Robert <millduos@gmail.com>
2020-02-25 13:43:11 +0800

    Access

--- access/ContentView.swift
+++ access/ContentView.swift
@@ -8,14 +8,52 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @ObservedObject var networkingManager: NetworkManaged = NetworkManaged()
     var body: some View {
-        Text("Hello, World!")
+        List(networkingManager.userList) {
+            userRowView(user: $0)
+        }
+    }
+}
+struct ImageViewWidget:View
+{
+    @ObservedObject var imageLoader: ImageLoader
+    init(url:String)
+    {
+        imageLoader = ImageLoader(imageURL: url)
+    }
+    var body:some View
+    {
+        Image(uiImage: UIImage(data: imageLoader.data) ?? UIImage()).resizable().frame(width: 40, height: 40).cornerRadius(20.0)
+    }
+}
+struct userRowView:View
+{
+    var user:UserEntry
+    var body:some View{
+        VStack{
+            HStack {
+                ImageViewWidget(url: user.avatar_url)
+                VStack(alignment: .leading, spacing: 1){
+                    Text(user.login).foregroundColor(Color.gray)
+                    if user.site_admin == true {
+                        Text("Admin").foregroundColor(Color.gray).fixedSize().frame(width: 50, height: 30, alignment: .center).background(Color.init(.purple)).cornerRadius(25)
+                    }else{
+                        Text("User").foregroundColor(Color.gray).fixedSize().frame(width: 50, height: 30, alignment: .center).background(Color.init(.green)).cornerRadius(25)
+
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
---
+++ access/user.swift
@@ -0,0 +1,92 @@
+//
+//  File.swift
+//  access
+//
+//  Created by 楊仁翰 on 2020/2/24.
+//  Copyright © 2020 Renhen Yang. All rights reserved.
+//
+import Foundation
+import Combine
+import SwiftUI
+
+struct UserEntry: Decodable, Identifiable{
+    let id: Int
+    let avatar_url: String
+    let login: String
+    let site_admin: Bool
+}
+
+//struct UserAPI: Decodable {
+//    var results: [UserEntry]
+//}
+
+class NetworkManaged: ObservableObject {
+    @Published var userList: [UserEntry] = [UserEntry]()
+
+    init() {
+        let urlString = "https://api.github.com/users?since=135"
+        guard let url = URL(string: urlString) else{return}
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            if let err = err {
+                print(err)
+                return
+            }
+            do {
+                let users = try JSONDecoder().decode([UserEntry].self, from: data!)
+                DispatchQueue.main.async {
+                    self.userList = users
+                    print(users)
+                }
+            } catch let jsonError {
+                print(jsonError)
+                return
+            }
+        }.resume()
+    }
+    
+}
+
+
+
+
+class ImageLoader:ObservableObject
+{
+    @Published var data:Data = Data()
+    func getImage(imageURL:String) {
+        guard let url = URL(string: imageURL) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                if let data = data {
+                    self.data = data
+                }
+            }
+            print(data!)
+        }.resume()
+    }
+    init(imageURL:String) {
+        getImage(imageURL: imageURL)
+    }
+}
+
+
+
+//func fatchUser(completion: @escaping (Result<[User], Error>) -> ()) {
+//    let urlString = "https://api.github.com/users?since=135"
+//    guard let url = URL(string: urlString) else{return}
+//    URLSession.shared.dataTask(with: url) { (data, resp, err) in
+//        if let err = err {
+//            completion(.failure(err))
+//            return
+//        }
+//        do {
+//            let users = try JSONDecoder().decode([User].self, from: data!)
+//            completion(.success(users))
+//
+//        } catch let jsonError {
+//            completion(.failure(jsonError))
+//        }
+//    }.resume()
+//
+//
+//}
